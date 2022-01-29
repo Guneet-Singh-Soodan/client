8389e07f587b174bc1dc4d52554d62b7243e72f8
Guneet-Singh-Soodan <gssoodan3281@gmail.com>
2022-01-29 20:43:48 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#"><img src='./images/navbar-icon.png'></img> spirals</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"><i class="fal fa-bars fa-2x"></i></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
